856c26e90c392ca1ad456c4bd74e749ff3c836a6
George <mflash91@gmail.com>
2017-11-25 23:29:48 +0500

    refactor balance game

--- src/common.js
+++ src/common.js
@@ -2,6 +2,54 @@ import readlineSync from 'readline-sync';
 
 const getRandomInt = (min, max) => Math.floor(Math.random() * ((max + 1) - min)) + min;
 
+const getLengthOfNumber = (number) => {
+  const func = (n, length) => {
+    if (n >= 1) {
+      return func(n / 10, length + 1);
+    }
+
+    return length;
+  };
+
+  return func(number, 0);
+};
+
+const getSumOfDigits = (number) => {
+  const func = (n, sum) => {
+    if (n > 0) {
+      const addToSum = n % 10;
+
+      return func(Math.floor(n / 10), sum + addToSum);
+    }
+
+    return sum;
+  };
+
+  return func(number, 0);
+};
+
+const balanceNumber = (number) => {
+  const sum = getSumOfDigits(number);
+  const count = getLengthOfNumber(number);
+
+  const modulo = sum % count;
+  const firstBalancer = Math.floor(sum / count);
+
+  const balance = (fstBalCount, scndBalCount, result) => {
+    if (fstBalCount > 0) {
+      return balance(fstBalCount - 1, scndBalCount, `${result}${firstBalancer}`);
+    }
+
+    if (scndBalCount > 0) {
+      return balance(0, scndBalCount - 1, `${result}${firstBalancer + 1}`);
+    }
+
+    return result;
+  };
+
+  return balance(count - modulo, modulo, '');
+};
+
 const isDivisor = (a, b, divisor) => !(a % divisor) && !(b % divisor);
 
 const askUser = question => readlineSync.question(question);
@@ -20,4 +68,5 @@ const reportWinning = name => console.log(`Congratulations, ${name}!`);
 
 const reportLosing = name => console.log(`Let's try again, ${name}!`);
 
-export { greetUser, askUser, reportWinning, reportLosing, welcome, getRandomInt, isDivisor };
+export { greetUser, askUser, reportWinning, reportLosing, welcome };
+export { getRandomInt, isDivisor, getLengthOfNumber, getSumOfDigits, balanceNumber };
--- src/games-logic/balance.js
+++ src/games-logic/balance.js
@@ -1,44 +1,14 @@
-import { cons, car, cdr } from 'hexlet-pairs';
-import { getRandomInt } from '../common';
+import { cons } from 'hexlet-pairs';
+import { getRandomInt, balanceNumber } from '../common';
 import processGame from '../game-processor';
 
 const gameTask = 'Balance the given number.';
 
 const getRoundData = () => {
-  const count = getRandomInt(2, 4);
+  const number = getRandomInt(10, 10000);
+  const answer = balanceNumber(number);
 
-  const getQuestionData = (index, sum, fullNumber) => {
-    if (index < count) {
-      const number = getRandomInt(0, 9);
-
-      return getQuestionData(index + 1, sum + number, `${fullNumber}${number}`);
-    }
-
-    return cons(sum, fullNumber);
-  };
-
-  const questionData = getQuestionData(0, 0, '');
-
-  const sum = car(questionData);
-  const modulo = sum % count;
-  const firstBalancer = Math.floor(sum / count);
-
-  const getAnswer = (fstBalCount, scndBalCount, result) => {
-    if (fstBalCount > 0) {
-      return getAnswer(fstBalCount - 1, scndBalCount, `${result}${firstBalancer}`);
-    }
-
-    if (scndBalCount > 0) {
-      return getAnswer(0, scndBalCount - 1, `${result}${firstBalancer + 1}`);
-    }
-
-    return result;
-  };
-
-  const question = cdr(questionData);
-  const answer = getAnswer(count - modulo, modulo, '');
-
-  return cons(question, answer);
+  return cons(`${number}`, answer);
 };
 
 const process = () => {
